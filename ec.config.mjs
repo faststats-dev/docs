c6f77df6def866929a2b4f2bead2f38373d4747b
Luca <lucameopou@gmail.com>
2026-07-08 14:37:05 +0200

    refactor: update theme configurations and clean up global styles

--- ec.config.mjs
+++ ec.config.mjs
@@ -7,8 +7,11 @@ export default defineEcConfig({
 	},
 	themes: ['dark-plus', 'light-plus'],
 	styleOverrides: {
+		borderRadius: '0px',
+		borderColor: 'var(--sl-color-hairline)',
 		frames: {
 			editorActiveTabIndicatorBottomColor: 'var(--sl-color-accent)',
-		}
+			frameBoxShadowCssValue: 'none',
+		},
 	},
 });
